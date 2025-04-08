78ff5e35452e4f77642297c1ae3db46e3a956dca
Kyle Willoughby <Kyle.M.Willoughby@Gmail.com>
2025-04-08 01:01:59 -0400

    Update GitHub Actions Workflow

--- StatVision/settings.py
+++ StatVision/settings.py
@@ -158,6 +158,3 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-RAPIDAPI_KEY = os.getenv('RAPIDAPI_KEY')
